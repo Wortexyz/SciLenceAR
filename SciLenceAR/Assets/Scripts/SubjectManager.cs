a0ede9bc0e06b6dfad60f05314d240a5d01e5b1c
MuhammedAzhar-Hub <muhammedashehar@gmail.com>
2025-11-26 16:59:49 +0530

    subjectManager

--- SciLenceAR/Assets/Scripts/SubjectManager.cs
+++ SciLenceAR/Assets/Scripts/SubjectManager.cs
@@ -1,27 +1,51 @@
 using UnityEngine;
-using Unity.Collections;
 
 public class SubjectManager : MonoBehaviour
 {
     public static SubjectManager Instance;
 
-    [Header("Subject Panels")]
+    [Header("Panels")]
+    public GameObject subjectPanel;    // The main subject selection panel (physics/chemistry/biology buttons)
     public GameObject physicsPanel;
     public GameObject chemistryPanel;
     public GameObject biologyPanel;
 
     private void Awake()
     {
-        Instance = this;
+        // Simple singleton (keeps the first instance, destroys duplicates)
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        // Start by showing the subject selection panel by default
+        OpenSubjectPanel();
     }
 
     private void HideAll()
     {
+        if (subjectPanel) subjectPanel.SetActive(false);
         if (physicsPanel) physicsPanel.SetActive(false);
         if (chemistryPanel) chemistryPanel.SetActive(false);
         if (biologyPanel) biologyPanel.SetActive(false);
     }
 
+    // Opens the main subject selection panel
+    public void OpenSubjectPanel()
+    {
+        HideAll();
+        if (subjectPanel) subjectPanel.SetActive(true);
+    }
+
+    // Subject openers (can be used by the subject buttons)
     public void OpenPhysics()
     {
         HideAll();
@@ -38,5 +62,11 @@ public class SubjectManager : MonoBehaviour
     {
         HideAll();
         if (biologyPanel) biologyPanel.SetActive(true);
+    }
+
+    // Call this from any subject panel's Back button to return to the subject selection panel.
+    public void BackToSubjectPanel()
+    {
+        OpenSubjectPanel();
     }
 }
