4ed204144d2d3527321a4b1424f33218bd70f5ab
Wortexyz <abhijithabhinav144@gmail.com>
2025-12-16 14:55:49 +0300

    AR scene initialized

---
+++ SciLenceAR/Assets/Scripts/AR/ARPlacementManager.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementManager : MonoBehaviour
+{
+    public ARRaycastManager raycastManager;
+    public GameObject placementPrefab;
+
+    private GameObject spawnedObject;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase != TouchPhase.Began)
+            return;
+
+        if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(placementPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
---
+++ SciLenceAR/Assets/Scripts/AR/ARSceneTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ARSceneTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadARScene()
+    {
+        SceneManager.LoadScene("ARScene");
+    }
+}
